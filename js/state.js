67fbde5d0a44c3fbf897ae78bf01ceb2e87a16fe
eiritwt <100699097+eiritwt@users.noreply.github.com>
2025-12-18 21:52:26 -0600

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,24 @@
+export const state = {
+    act: 'act1',
+    body: '', mouth: '', eyes: '', special: 'none',
+    isLooping: false, vibrate: false, flipped: false,
+
+    getRestrictions() {
+        const isR = (v) => v.includes('_r') || v.includes('_right');
+        const isCry = (v) => v.includes('cry') && !isR(v);
+        const isJudge = (v) => v.includes('judge');
+
+        let result = { lockEyes: null, lockMouth: null, disableMouth: () => false };
+
+        if (isJudge(this.body) || isJudge(this.eyes)) {
+            result.lockEyes = 'eyes_judge';
+            if (isCry(this.mouth) || isR(this.mouth)) result.lockMouth = 'mouth_normal';
+            result.disableMouth = (m) => isCry(m) || isR(m);
+        } else if (isCry(this.body) || isCry(this.mouth) || isCry(this.eyes)) {
+            result.disableMouth = (m) => !isCry(m);
+        } else if (isR(this.mouth)) {
+            result.lockEyes = 'eyes_normal_r';
+        }
+        return result;
+    }
+};

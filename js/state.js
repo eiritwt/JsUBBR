dcfb3cae366485c970e62d64f803180fd3c5c235
eiritwt <100699097+eiritwt@users.noreply.github.com>
2025-12-18 22:04:53 -0600

    Update state.js

--- js/state.js
+++ js/state.js
@@ -4,16 +4,17 @@ export const state = {
     isLooping: false, vibrate: false, flipped: false,
 
     getRestrictions() {
-        const isR = (v) => v.includes('_r') || v.includes('_right');
-        const isCry = (v) => v.includes('cry') && !isR(v);
-        const isJudge = (v) => v.includes('judge');
+        const isR = (v) => v && (v.includes('_r') || v.includes('_right'));
+        const isCry = (v) => v && v.includes('cry') && !isR(v);
+        const isJudge = (v) => v && v.includes('judge');
 
-        let result = { lockEyes: null, lockMouth: null, disableMouth: () => false };
+        let result = { lockEyes: null, lockMouth: null, disableMouth: () => false, disableEyes: () => false };
 
         if (isJudge(this.body) || isJudge(this.eyes)) {
             result.lockEyes = 'eyes_judge';
             if (isCry(this.mouth) || isR(this.mouth)) result.lockMouth = 'mouth_normal';
             result.disableMouth = (m) => isCry(m) || isR(m);
+            result.disableEyes = (e) => !isJudge(e);
         } else if (isCry(this.body) || isCry(this.mouth) || isCry(this.eyes)) {
             result.disableMouth = (m) => !isCry(m);
         } else if (isR(this.mouth)) {
